4d10bf8d4fbc204920c38304e5f2d3b43ea8be9c
mbneobrix <mbcse50@gmail.com>
2023-04-15 13:39:05 +0900

    Added cross chain handler

---
+++ Contracts/interfaces/IConnextHandler.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity 0.8.17;
+
+import {IExecutor} from "../interfaces/IExecutor.sol";
+
+struct CallParams {
+    address to;
+    bytes callData;
+    uint32 originDomain;
+    uint32 destinationDomain;
+    address recovery;
+    address callback;
+    uint256 callbackFee;
+    bool forceSlow;
+    bool receiveLocal;
+}
+
+struct XCallArgs {
+    CallParams params;
+    address transactingAssetId; // Could be adopted, local, or wrapped
+    uint256 amount;
+    uint256 relayerFee;
+}
+
+interface IConnextHandler {
+    function executor() external view returns (IExecutor);
+
+    function xcall(XCallArgs calldata) external;
+}
